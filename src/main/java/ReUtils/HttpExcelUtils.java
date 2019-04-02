db31f3c60658d394d469d53cd0ba3b5b9edc1b0c
daiworkspaces <xmcb008@sina.com>
2019-04-02 22:25:35 +0800

    V6 CaseV2

--- src/main/java/ReUtils/HttpExcelUtils.java
+++ src/main/java/ReUtils/HttpExcelUtils.java
@@ -2,38 +2,64 @@ package ReUtils;
 
 import org.apache.poi.ss.usermodel.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class HttpExcelUtils {
 
-    public static void main(String[] args) throws IOException {
+    public static Object[][] read(int[] rows,int[] cells) throws FileNotFoundException {
 
 
         /**
          * 单个单元格
          */
         InputStream inputStream = new FileInputStream(new  File("src\\main\\resources\\接口测试用例V1.0.xlsx"));
-        Workbook workbook = WorkbookFactory.create(inputStream);
+        Workbook workbook = null;
+        try {
+            workbook = WorkbookFactory.create(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        Object[][] datas = new Object[rows.length][cells.length];
         Sheet sheet = workbook.getSheet("接口信息");
-//        Row row = sheet.getRow(1);
-//
-//        Cell cell = row.getCell(4, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-//        cell.setCellType(CellType.STRING);
-//        String cellValue = cell.getStringCellValue();
-//        System.out.println(cellValue);
+        for (int i = 0; i < rows.length; i++) {
+            Row row = sheet.getRow(rows[i]-1);//行
+
+
+            for (int j = 0; j < cells.length; j++) {
+
+                Cell cell = row.getCell(cells[j]-1, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                cell.setCellType(CellType.STRING);
+
+
+
+                String cellValue =cell.getStringCellValue();
+                System.out.println(cellValue);
+                datas[i][j] =cellValue;
+
+                if (inputStream !=null){
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+        return datas;
+
+
+
+
+
+
+
 
 
     }
 
 
-    /**
-     *  指定多个单元格
-     *
-     */
 
 
 
--- src/main/java/apicase/Cases4.java
+++ src/main/java/apicase/Cases4.java
@@ -1,13 +1,18 @@
 package apicase;
 
+import ReUtils.HttpExcelUtils;
 import ReUtils.HttpRequest2;
 import ReUtils.HttpRequestUtils;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Cases4 {
 
@@ -58,7 +63,7 @@ public class Cases4 {
 
 
 
-    @Test (dataProvider = "datas")
+    @Test (dataProvider = "d1")
     public void LoginCase2(String ACB051,String UCC003){
         String url ="http://47.104.134.50:8666/ManagementPlatform/Account/Admin/v1.0/PhoneLogin";
         //String ACB501="13522352342";
@@ -72,6 +77,45 @@ public class Cases4 {
 
 
     }
+    @DataProvider(name = "d1")
+    public Object[][]  testexceldata() throws FileNotFoundException {
+        int[] rows ={2,3,4,5};
+        int[] cells ={6};
+        Object[][] testexceldata = HttpExcelUtils.read(rows,cells);
+       //System.out.println(testexceldata.toString());
+
+        return testexceldata;
+    }
+
+
+
+    @Test (dataProvider = "d1")
+    public void LoginCase3(String parametsJson){
+        String url ="http://47.104.134.50:8666/ManagementPlatform/Account/Admin/v1.0/PhoneLogin";
+        //String ACB501="13522352342";
+        //String UCC003="E10ADC3949BA59ABBE56E057F20F883E";
+
+        List<BasicNameValuePair> p2arm = new ArrayList<BasicNameValuePair>();
+
+        Map<String,String> jsonMap =(Map<String, String>) JSONObject.parse(parametsJson);
+        Set<String> paramNames = jsonMap.keySet();//获取所有的key
+        for (String paramName:paramNames){
+           String paramValue = jsonMap.get(paramName);//通过key获取名
+         p2arm.add(new  BasicNameValuePair(paramName,paramValue));
+        }
+
+//
+//        p2arm.add(new BasicNameValuePair("ACB501",ACB051));//属于硬编码
+//        p2arm.add(new BasicNameValuePair("UCC003",UCC003));
+
+        httpRequest2.do_testByPost(url,p2arm);
+
+
+    }
+
+
+
+
 
 
 
